0f54869841531e9c5a98965d9e3b8eef51f702a7
Ruslan Zakirov <ruz@bestpractical.com>
2011-01-19 17:20:19 +0300

    fixes in exporter

--- lib/RT/App/Export.pm
+++ lib/RT/App/Export.pm
@@ -206,7 +206,7 @@ sub follow_cgm_self {
         )
     ";
     return $self->push_collection(
-        class => 'RT::CachedGroupMembers',
+        class => 'RT::CachedGroupMember',
         alias => 'cgm',
         query => $query,
         binds => $args{'binds'},
@@ -305,7 +305,7 @@ sub follow_transaction_values {
             )
         ";
         $self->push_collection(
-            class => 'RT::Principals',
+            class => 'RT::Principal',
             alias => 'p',
             query => $query,
             binds => [ @{ $args{'binds'} }, qw(AddWatcher DelWatcher) ],
@@ -322,7 +322,7 @@ sub follow_transaction_values {
             )
         ";
         $self->push_collection(
-            class => 'RT::Links',
+            class => 'RT::Link',
             alias => 'l',
             query => $query,
             binds => [ @{ $args{'binds'} }, qw(AddLink DeleteLink) ],
@@ -398,82 +398,6 @@ sub follow_links {
             binds => $binds,
         );
     }
-}
-
-sub export {
-    my $self = shift;
-    my %args = (
-        class => undef,
-        alias => undef,
-        query => undef,
-        binds => [],
-        @_
-    );
-
-    my @fields = $self->fields( class => $args{'class'} );
-
-    my $seen = $self->{'seen'}{ $args{'class'} } ||= {};
-
-    my ($fh, $csv) = $self->csv( %args );
-
-    my $query = 'SELECT '. join(', ', map "$args{'alias'}.$_", @fields)
-        .' FROM '. $args{'query'};
-    print "$query\n";
-    print "\tbindings: ". join(', ', map "'$_'", @{ $args{'binds'} }) . "\n"
-        if @{ $args{'binds'} };
-
-    my $sth = $RT::Handle->SimpleQuery( $query, @{ $args{'binds'} } );
-    unless ( $sth ) {
-        die "Couldn't execute the query: ". $sth->error_message;
-    }
-
-    my $counter = 0;
-    while ( my $row = $sth->fetchrow_arrayref ) {
-        next if exists $seen->{ $row->[0] };
-        $seen->{ $row->[0] } = undef;
-        $counter++;
-
-        $csv->print($fh, $row);
-    }
-    print "Exported $counter rows\n";
-    return $counter;
-}
-
-sub csv {
-    my $self = shift;
-    my %args = (
-        class => undef,
-        @_
-    );
-
-    my $name = lc $args{'class'};
-    $name =~ s/:/_/g;
-    my $path = File::Spec->catfile( $self->output, $name . '.csv' );
-    my $exists = -e $path;
-
-    open my $fh, '>>:raw', $path
-        or die "Couldn't open '$path': $!";
-
-    my $csv = Text::CSV->new ( { binary => 1 } );
-
-    unless ( $exists ) {
-        print $fh '# RT '. $RT::VERSION .' '. $args{'class'} ."\n";
-        $csv->print( $fh, [ $self->fields( class => $args{'class'} ) ] );
-    }
-    return ($fh, $csv);
-}
-
-sub fields {
-    my $self = shift;
-    my %args = (@_);
-
-    return sort { $a eq 'id'? -1 : $a cmp $b } $RT::Handle->Fields( $self->table( %args ) );
-}
-
-sub table {
-    my $self = shift;
-    my %args = (@_);
-    return $args{'class'}->new( $RT::SystemUser )->Table;
 }
 
 =head1 AUTHOR
